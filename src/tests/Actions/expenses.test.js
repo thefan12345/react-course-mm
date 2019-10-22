8298d9a0b77b6f53b3e1ba6f7eb230da48295e2d
thefan12345 <mikematsoukas@msn.com>
2019-10-22 06:31:25 +0100

    remove expense from firebase

--- src/tests/Actions/expenses.test.js
+++ src/tests/Actions/expenses.test.js
@@ -1,4 +1,13 @@
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import {
+    startAddExpense,
+    addExpense,
+    editExpense,
+    removeExpense,
+    setExpenses,
+    startSetExpenses,
+    startRemoveExpense
+} from '../../actions/expenses';
+
 import expenses from '../fixtures/expenses';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
@@ -22,6 +31,22 @@ test('Should setup Remove expense action object', () => {
         id: '123abc'
     });
 });
+
+test('should remove expense from firebase', (done) => {
+    const store = createMockStore({});
+    const id = expenses[2].id;
+    store.dispatch(startRemoveExpense({ id })).then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id
+      });
+      return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+      expect(snapshot.val()).toBeFalsy();
+      done();
+    });
+  });
 
 test('Should setup edit expense action object', () => {
     const action = editExpense( '123abc', { note: 'New value' });
